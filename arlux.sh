851581f1e798603ade123f219a554a4f058f1f63
3245as <salehff00@gmail.com>
2017-03-03 20:40:22 +0330

    Update and rename beyond.sh to arlux.sh

--- arlux.sh
+++ arlux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDReborn
+cd $HOME/Arlux
 
 install() {
 	    cd tg
@@ -31,13 +31,13 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
 fi
    echo -e "\033[38;5;208m"
-   echo -e "     > BeyondTeam Source :D                        "
+   echo -e "     > ArluxTeam Source :D                        "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
 fi
 
-# Now All Argument Support after ./beyond.sh !
+# Now All Argument Support after ./arlux.sh !
 #	Arguments :
 #			#			#			#			#			#			#			#			#			#
 #  --phone/-u                           specify username (would not be asked during authorization)
@@ -67,8 +67,8 @@ fi
 #  --permanent-peer-ids                 use permanent peer ids
 #			#			#			#			#			#			#			#			#			#
 #Example To launch with second profile :
-# ./beyond.sh -p second-profile
+# ./arlux.sh -p second-profile
 
 #			  OR
 
-# ./beyond.sh --profile second-profile
+# ./arlux.sh --profile second-profile
